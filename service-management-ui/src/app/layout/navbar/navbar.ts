cbb8b0f1011c609e793d4ba2dba7d8ea596778a1
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2026-01-05 10:39:45 +0530

    added dropdowns

--- service-management-ui/src/app/layout/navbar/navbar.ts
+++ service-management-ui/src/app/layout/navbar/navbar.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ClickOutsideDirective } from '../../shared/directives/click-outside.directive';
+import { AuthModalService } from '../../shared/services/auth-modal.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ClickOutsideDirective
+  ],
   templateUrl: './navbar.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  private authModal = inject(AuthModalService);
+
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen = false;
+  }
+
+  openLogin() {
+    this.closeDropdown();
+    this.authModal.open('login');
+  }
+
+  openRegister() {
+    this.closeDropdown();
+    this.authModal.open('register');
+  }
+}
